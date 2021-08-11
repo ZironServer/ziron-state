0e229c52f778ca6260d67826be76eac1ed1f5ae2
Luca Scaringella <lucalstyle@gmail.com>
2021-08-11 21:23:30 +0200

    Add more environment variables in the run file.

--- src/run.ts
+++ src/run.ts
@@ -6,10 +6,15 @@ Copyright(c) Luca Scaringella
 
 import {StateServer} from "./lib/StateServer";
 import { secrets } from "docker-secret";
+import {LogLevel} from "./lib/Logger";
 
 const variables = Object.assign({}, process.env, secrets);
 
 process.title = `Ziron State`;
 new StateServer({
-    secret: variables.SECRET
+    secret: variables.SECRET,
+    port: parseInt(variables.PORT) || 7777,
+    path: variables.PATH || "/",
+    logLevel: parseInt(variables.LOG_LEVEL) || LogLevel.Everything,
+    scaleDelay: parseInt(variables.SCALE_DELAY) || 100
 }).listen();
