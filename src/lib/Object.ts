ff97b21a33e4ddaa5d28e22e7017db0b70239411
Luca Scaringella <lucalstyle@gmail.com>
2021-10-22 12:44:17 +0200

    Add copyright notice.

--- src/lib/Object.ts
+++ src/lib/Object.ts
@@ -1,3 +1,9 @@
+/*
+Author: Ing. Luca Gian Scaringella
+GitHub: LucaCode
+Copyright(c) Ing. Luca Gian Scaringella
+ */
+
 export function buildOptions<T>(defaults: Required<T>, options: Partial<T>): Required<T> {
     for(let k in options) {
         if(options.hasOwnProperty(k) && defaults.hasOwnProperty(k) && options[k] !== undefined) defaults[k] = options[k]!;
