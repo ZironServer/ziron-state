15ed76a1a03ba47dee04480def7008828ab3fbce
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-04-20 21:50:13 +0200

    Export StateServerOptions.

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,10 @@ Copyright(c) Ing. Luca Gian Scaringella
 
 import {StateServer} from "./lib/StateServer";
 import {LogLevel} from "./lib/Logger";
+import StateServerOptions from "./lib/StateServerOptions";
 
 export {
     StateServer,
+    StateServerOptions,
     LogLevel
 }
